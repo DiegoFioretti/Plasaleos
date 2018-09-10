bc220809a4236d56a316bbdf6b09c492d4395c62
Marchin <mmonterrosa94@gmail.com>
2018-09-09 23:34:19 -0300

    Update Aliens
    Mejoras en el movimiento de los aliens y en el diseño de su interfaz

--- Plasaleos/Assets/Scripts/Alien/Alien.cs
+++ Plasaleos/Assets/Scripts/Alien/Alien.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Movement))]
 public class Alien : MonoBehaviour {
     IState m_currState;
+    IState m_nextState;
 
     private void Awake() {
         m_currState = GetComponent<Movement>();
     }
 
     private void Update() {
-        m_currState = m_currState.StateUpdate();
+        m_currState.StateUpdate(ref m_nextState);
+        m_currState = m_nextState;
     }
 
     private void FixedUpdate() {
