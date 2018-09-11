03580f4cafce5e06023944c87342b7f1804c2791
Marchin <mmonterrosa94@gmail.com>
2018-09-11 10:38:17 -0300

    Agregar estructura básica de la UI placeholder

---
+++ Plasaleos/Assets/Scripts/PlaceHolderUIManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaceHolderUIManager : MonoBehaviour {
+
+    public void LoadLevel1() {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadLevel2() {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void LoadLevel3() {
+        SceneManager.LoadScene("Level3");
+    }
+
+}
