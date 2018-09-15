dc2ab9331231cb3df51ffbd987c9e28e2c6b70d0
Marchin <mmonterrosa94@gmail.com>
2018-09-15 12:33:42 -0300

    Arreglar Merge de Alien
    Remover archivos excedentes
    Configurar apropiadamente

--- Plasaleos/Assets/Scripts/Entidades/IState.cs
+++ Plasaleos/Assets/Scripts/Entidades/IState.cs
@@ -1,8 +1,4 @@
 public interface IState {
-<<<<<<< HEAD:Plasaleos/Assets/Scripts/Alien/IState.cs
-    void StateUpdate(ref IState currState);
-=======
     void StateUpdate(out IState nextState);
->>>>>>> Alien:Plasaleos/Assets/Scripts/Entidades/IState.cs
     void StateFixedUpdate();
 }
