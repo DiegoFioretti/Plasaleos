6d9e12023fd6b3864515c7a9edd71bb17e888f63
Marchin <mmonterrosa94@gmail.com>
2018-09-26 01:09:33 -0300

    Agregar estado de animal: Rest

---
+++ Plasaleos/Assets/Scripts/Entidades/Animal/Rest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Rest : MonoBehaviour, IState {
+    [SerializeField] float m_speedToAwake;
+    Entity m_entity;
+    bool m_awaken;
+
+    private void Awake() {
+        m_entity = GetComponent<Entity>();
+    }
+
+    private void OnEnable() {
+        m_awaken = false;
+    }
+
+    public void StateUpdate(out IState nextState) {
+        if (m_entity.m_rb.velocity.magnitude > m_speedToAwake) {
+            m_awaken = true;
+        }
+        if (m_awaken) {
+            nextState = GetComponent<Movement>();
+        } else {
+            nextState = this;
+        }
+    }
+
+    public void StateFixedUpdate() { }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.GetComponent<Alien>()) {
+            m_awaken = true;
+        }
+    }
+}
