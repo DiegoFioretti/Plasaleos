11aee941cc6fe177c41f1a178786aa9d3ba3175a
Marchin <mmonterrosa94@gmail.com>
2018-09-26 01:09:52 -0300

    Agregar estado de animal: Devour

---
+++ Plasaleos/Assets/Scripts/Entidades/Animal/Devour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animal))]
+public class Devour : MonoBehaviour, IState {
+    [SerializeField] float m_duration;
+    Animal m_animal;
+    float m_timer;
+
+    private void Awake() {
+        m_animal = GetComponent<Animal>();
+    }
+
+    private void OnEnable() {
+        m_timer = m_duration;
+    }
+
+    public void StateUpdate(out IState nextState) {
+        m_timer -= Time.deltaTime;
+        if (m_timer < 0f) {
+            RaycastHit2D hit;
+            if (m_animal.SearchPrey(out hit)) {
+                Hunt hunt = GetComponent<Hunt>();
+                hunt.Chase(hit.transform);
+                nextState = hunt;
+            } else {
+                nextState = GetComponent<Movement>();
+            }
+        } else {
+            nextState = this;
+        }
+    }
+
+    public void StateFixedUpdate() { }
+}
