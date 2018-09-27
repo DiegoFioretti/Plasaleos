c544507040e6059ffdd9399baa57c0afbe9769c5
Marchin <mmonterrosa94@gmail.com>
2018-09-27 20:12:36 -0300

    Arreglar bugs Animal
    Arreglar bug de animal persiguiendo a alien muerto
    Arreglar bug de animal no perdiendo a alien tras traspasar cierta distancia entre ellos

--- Plasaleos/Assets/Scripts/Entidades/Animal/Hunt.cs
+++ Plasaleos/Assets/Scripts/Entidades/Animal/Hunt.cs
@@ -4,6 +4,7 @@
 public class Hunt : MonoBehaviour, IState {
     [SerializeField] float m_chaseSpeed;
     [SerializeField] float m_reachDistance;
+    [SerializeField] float m_looseDistance;
     Animal m_animal;
     Movement m_movement;
     Alien m_alienPrey;
@@ -22,17 +23,23 @@ public class Hunt : MonoBehaviour, IState {
             m_animal.Flip();
         }
         RaycastHit2D hit;
-        if (m_animal.SearchPrey(out hit)) {
-            if (hit.distance < m_reachDistance) {
-                m_alienPrey.Damage();
-                nextState = GetComponent<Devour>();
+        if (!m_alienPrey.isActiveAndEnabled || (Vector3.Distance(
+                transform.position, m_alienPrey.transform.position) > m_looseDistance)) {
+
+            nextState = GetComponent<Movement>();
+        } else {
+            if (m_animal.SearchPrey(out hit)) {
+                if (hit.distance < m_reachDistance) {
+                    m_alienPrey.Damage();
+                    nextState = GetComponent<Devour>();
+                } else {
+                    m_alienPrey = hit.transform.GetComponent<Alien>();
+                    m_alienPrey.Scare(m_animal.FacingRight);
+                    nextState = this;
+                }
             } else {
-                m_alienPrey = hit.transform.GetComponent<Alien>();
-                m_alienPrey.Scare(m_animal.FacingRight);
                 nextState = this;
             }
-        } else {
-            nextState = this;
         }
     }
 
