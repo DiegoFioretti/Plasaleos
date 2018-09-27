4262130ca4366315898e9335f113e861467078ab
Matías Fernández <matiasfernandez602@gmail.com>
2018-09-27 17:34:04 -0300

    Nuevo sonido + arte UI

--- Plasaleos/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Plasaleos/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -18,6 +18,7 @@ namespace AK
         static const AkUniqueID ALIEN_STEP = 1249666675U;
         static const AkUniqueID HAZARD_BROKENICE = 1529951376U;
         static const AkUniqueID MUSIC_BACKGROUND = 3998872777U;
+        static const AkUniqueID MUSIC_VICTORY = 2637400697U;
         static const AkUniqueID STOPALL = 3086540886U;
         static const AkUniqueID TRAP_HIT = 2814314654U;
     } // namespace EVENTS
