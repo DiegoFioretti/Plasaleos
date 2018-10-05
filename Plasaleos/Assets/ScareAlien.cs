19707d31ee940aee99045c6efeb8f50cc13de297
Matías Fernández <matiasfernandez602@gmail.com>
2018-10-05 16:11:01 -0300

    Ajuste de gravedad y botones de alerta y susto

---
+++ Plasaleos/Assets/ScareAlien.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScareAlien : MonoBehaviour {
+
+	public void ChangeToScare()
+    {
+        GameObject[] go = GameObject.FindGameObjectsWithTag("Alien");
+        for(int i = 0; i < go.Length; i++)
+        {
+            go[i].GetComponent<Alien>().Scare();
+        }
+    }
+}
