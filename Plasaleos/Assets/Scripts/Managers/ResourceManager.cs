5282bf5f8358fc9ee251432b0db78c09a92c2b97
Marchin <mmonterrosa94@gmail.com>
2018-10-14 19:28:47 -0300

    Extender ResourceManager para soportar otros recursos

--- Plasaleos/Assets/Scripts/Managers/ResourceManager.cs
+++ Plasaleos/Assets/Scripts/Managers/ResourceManager.cs
@@ -12,10 +12,19 @@ public class ResourceManager : MonoBehaviour {
     [HideInInspector]
     public UnityEvent ScareChange;
     int m_lianas;
+    int m_mushrooms;
+    int m_alerts;
+    int m_scares;
 
     private void Awake() {
         m_lianas = m_levelResources.lianas;
+        m_mushrooms = m_levelResources.mushrooms;
+        m_alerts = m_levelResources.alerts;
+        m_scares = m_levelResources.scares;
         LianaChange.Invoke();
+        MushroomChange.Invoke();
+        AlertChange.Invoke();
+        ScareChange.Invoke();
     }
 
     public bool RequestLiana() {
@@ -35,5 +44,62 @@ public class ResourceManager : MonoBehaviour {
 
     public int GetLianaAmount() {
         return m_lianas;
+    }
+
+    public bool RequestMushroom() {
+        if (m_mushrooms > 0) {
+            m_mushrooms--;
+            MushroomChange.Invoke();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void AddMushroom() {
+        m_mushrooms++;
+        MushroomChange.Invoke();
+    }
+
+    public int GetMushroomAmount() {
+        return m_mushrooms;
+    }
+
+    public bool RequestAlert() {
+        if (m_alerts > 0) {
+            m_alerts--;
+            AlertChange.Invoke();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void AddAlert() {
+        m_alerts++;
+        AlertChange.Invoke();
+    }
+
+    public int GetAlertAmount() {
+        return m_alerts;
+    }
+
+    public bool RequestScare() {
+        if (m_scares > 0) {
+            m_scares--;
+            ScareChange.Invoke();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void AddScare() {
+        m_scares++;
+        ScareChange.Invoke();
+    }
+
+    public int GetScareAmount() {
+        return m_scares;
     }
 }
