4b611708d9b359a1f29b9c4686d3bf917b91567b
Marchin <mmonterrosa94@gmail.com>
2018-10-18 10:58:30 -0300

    Crear roca

--- Plasaleos/Assets/POHit.cs
+++ Plasaleos/Assets/POHit.cs
@@ -8,18 +8,15 @@ public class POHit : MonoBehaviour {
     [SerializeField] private float minSpeed = 1;
     private Rigidbody2D rb;
 
-    private void Start()
-    {
+    private void Start() {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (rb.velocity.magnitude > minSpeed)
-        {
-            if (lm == (lm | (1 << collision.gameObject.layer)))
-            {
-                Destroy(collision.gameObject);
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (rb.velocity.magnitude > minSpeed) {
+            Entity entity;
+            if (entity = collision.gameObject.GetComponent<Entity>()) {
+                entity.Damage();
             }
         }
     }
