03f13d77d92620b2a867d5fed1d0546871102535
Marchin <mmonterrosa94@gmail.com>
2018-10-31 10:10:30 -0300

    Mejorar Perfomance del contador de aliens

--- Plasaleos/Assets/Scripts/UI/UISpritePos.cs
+++ Plasaleos/Assets/Scripts/UI/UISpritePos.cs
@@ -9,25 +9,25 @@ public class UISpritePos : MonoBehaviour {
     private Transform objectToFollow;
 
     private GameObject gyro;
+    private RectTransform rectTransform;
+    private GyroController gyroController;
 
-    private void Start()
-    {
+    private void Start() {
         healthBar = gameObject.GetComponent<RectTransform>();
         gyro = GameObject.FindGameObjectWithTag("Gyroscope");
+        gyroController = gyro.GetComponent<GyroController>();
+        rectTransform = GetComponent<RectTransform>();
         canvasRectT = GameObject.FindGameObjectWithTag("AlienCounterCanvas").GetComponent<RectTransform>();
         objectToFollow = GameObject.FindGameObjectWithTag("AlienCounterPoint").transform;
     }
 
-    void Update()
-    {
+    void Update() {
         Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(Camera.main, objectToFollow.position);
         healthBar.anchoredPosition = screenPoint - canvasRectT.sizeDelta / 2f;
-        if(gyro != null)
-        {
-            if (!gyro.GetComponent<GyroController>().dragGravity)
-            {
-                GetComponent<RectTransform>().up = gyro.transform.up;
-                GetComponent<RectTransform>().rotation = Quaternion.Euler(0, 0, GetComponent<RectTransform>().rotation.eulerAngles.z);
+        if (gyro != null) {
+            if (!gyroController.dragGravity) {
+                rectTransform.up = gyro.transform.up;
+                rectTransform.rotation = Quaternion.Euler(0, 0, rectTransform.rotation.eulerAngles.z);
             }
         }
     }
