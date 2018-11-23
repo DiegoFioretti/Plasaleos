9439d138f2e98c30f2b7e7150b6313f18314f34c
Matías Fernández <matiasfernandez602@gmail.com>
2018-11-23 01:47:55 -0300

    Candados, bloqueos, drag del mapa

---
+++ Plasaleos/Assets/Scripts/DragCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragCamera : MonoBehaviour
+{
+    [SerializeField] private Transform left;
+    [SerializeField] private Transform right;
+
+    public float speed = 0.1F;
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            transform.Translate(-touchDeltaPosition.x * speed, 0, 0);
+        }
+        if(transform.position.x < left.position.x)
+        {
+            transform.position = new Vector3(left.position.x, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > right.position.x)
+        {
+            transform.position = new Vector3(right.position.x, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Plasaleos/Assets/Scripts/ShowIfLocked.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowIfLocked : MonoBehaviour {
+
+    [SerializeField] private int unlock;
+    [SerializeField] private int requiredPieces;
+
+    // Use this for initialization
+    void Start () {
+        if (unlock <= GameManager.instance.AlienCount && requiredPieces <= GameManager.instance.PieceCount)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
