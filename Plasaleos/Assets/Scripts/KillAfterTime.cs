a67451cad5538c642300aaeb9c82a7ac466cece0
Matías Fernández <matiasfernandez602@gmail.com>
2018-12-07 12:30:53 -0300

    Feedback de alerta

---
+++ Plasaleos/Assets/Scripts/KillAfterTime.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillAfterTime : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("Kill", 2);
+	}
+
+    private void Kill()
+    {
+        Destroy(gameObject);
+    }
+}
