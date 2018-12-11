6e8766cbcb59f0fc72381f703e74b56ba70b62f0
Marchin <mmonterrosa94@gmail.com>
2018-12-11 09:25:25 -0300

    Arreglar tinte Background Restringido

--- Plasaleos/Assets/Scripts/Ambiente/GravityImageChanger.cs
+++ Plasaleos/Assets/Scripts/Ambiente/GravityImageChanger.cs
@@ -5,9 +5,11 @@ public class GravityImageChanger : MonoBehaviour {
     [SerializeField] Sprite m_unrestrictedSprite;
     [SerializeField] Sprite m_restrictedSprite;
     Image m_image;
+    Color m_blueColor;
 
     private void Awake() {
         m_image = GetComponent<Image>();
+        m_blueColor = m_image.color;
     }
 
     private void Start() {    
@@ -17,6 +19,8 @@ public class GravityImageChanger : MonoBehaviour {
     void ChangeRestriction() {
         m_image.sprite = (GravityController.Instance.Restricted?
             m_restrictedSprite : m_unrestrictedSprite);
+        m_image.color = (GravityController.Instance.Restricted?
+            Color.white : m_blueColor);
     }
 
 
