6a1ac53f595c033a81c1547b668239c4418f8d6f
Marchin <mmonterrosa94@gmail.com>
2018-12-10 22:27:04 -0300

    Hacer que el fondo cambie con los switch

---
+++ Plasaleos/Assets/Scripts/Ambiente/GravityImageChanger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GravityImageChanger : MonoBehaviour {
+    [SerializeField] Sprite m_unrestrictedSprite;
+    [SerializeField] Sprite m_restrictedSprite;
+    Image m_image;
+
+    private void Awake() {
+        m_image = GetComponent<Image>();
+    }
+
+    private void Start() {    
+        GravityController.Instance.RestrictionChange.AddListener(ChangeRestriction);
+    }
+
+    void ChangeRestriction() {
+        m_image.sprite = (GravityController.Instance.Restricted?
+            m_restrictedSprite : m_unrestrictedSprite);
+    }
+
+
+}
